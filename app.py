99eb28696e6d7b4a965a3ef87ad70f43cadd8eb2
millim1983 <kiakiakia83@gmail.com>
2024-07-22 15:51:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def main():
 
     #PIL 패키지에 이미지 모듈을 통해 이미지 열기 
     # Image.open('이미지 경로')
-    img = 'https://github.com/user-attachments/assets/8ae84f11-c6e9-482e-b55a-3e01d4a283e6'
+    img = 'https://github.com/millim1983/bigdata_mr/moldiv_familychair.png'
     logo_img = Image.open(img)
 
     col1,col2 = st.columns([2,3])
