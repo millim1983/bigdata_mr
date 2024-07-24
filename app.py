0acc2c0dec0cf3098a27141c45c8503f9cbe6e63
millim1983 <kiakiakia83@gmail.com>
2024-07-24 12:44:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ def main():
 
     # GitHub 리포지토리의 이미지 URL
     #url = "https://github.com/user-attachments/assets/57728188-0a49-4204-84dd-0c393d533a7e"
-    url = 'https://github.com/millim1983/bigdata_mr/moldiv_familychair.png'
-
+    #url = 'https://github.com/millim1983/bigdata_mr/moldiv_familychair.png'
+    url = 'https://github.com/millim1983/bigdata_mr/blob/main/images/moldiv_familychair.png?raw=true'
     try:
         # 이미지 URL로부터 이미지 데이터 가져오기
         response = requests.get(url)
